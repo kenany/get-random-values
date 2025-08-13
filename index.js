ddef386fc0bc842f1e6eed6beac8dea6947f3655
Kenan Yildirim <kenan@kenany.me>
2025-08-12 21:43:34 -0400

    feat: drop Node.js v18 support
    BREAKING CHANGE: Node.js v18 is no longer supported.

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-/* eslint-disable no-var, operator-linebreak */
-
-var window = require('global/window');
-var nodeCrypto = require('crypto');
+const window = require('global/window');
+const nodeCrypto = require('node:crypto');
 
 /**
- * @template {ArrayBufferView | null} T
+ * @template {ArrayBufferView<ArrayBufferLike>} T
  * @param {T} buf
  * @returns {T}
  */
 function getRandomValues(buf) {
-  if (window.crypto && window.crypto.getRandomValues) {
+  if (window.crypto?.getRandomValues) {
     return window.crypto.getRandomValues(buf);
   }
-  if (typeof window.msCrypto === 'object' && typeof window.msCrypto.getRandomValues === 'function') {
+
+  if (
+    typeof window.msCrypto === 'object' &&
+    typeof window.msCrypto.getRandomValues === 'function'
+  ) {
     return window.msCrypto.getRandomValues(buf);
   }
+
   if (nodeCrypto.randomBytes) {
     if (!(buf instanceof Uint8Array)) {
       throw new TypeError('expected Uint8Array');
     }
-    if (buf.length > 65536) {
-      var e = new Error();
+    if (buf.length > 65_536) {
+      const e = new Error(
+        "Failed to execute 'getRandomValues' on 'Crypto': The " +
+          "ArrayBufferView's byte length (" +
+          buf.length +
+          ') exceeds the ' +
+          'number of bytes of entropy available via this API (65536).'
+      );
       // @ts-expect-error
       e.code = 22;
-      e.message = 'Failed to execute \'getRandomValues\' on \'Crypto\': The ' +
-        'ArrayBufferView\'s byte length (' + buf.length + ') exceeds the ' +
-        'number of bytes of entropy available via this API (65536).';
       e.name = 'QuotaExceededError';
       throw e;
     }
-    var bytes = nodeCrypto.randomBytes(buf.length);
+    const bytes = nodeCrypto.randomBytes(buf.length);
     buf.set(bytes);
     return buf;
   }
-  else {
-    throw new Error('No secure random number generator available.');
-  }
+
+  throw new Error('No secure random number generator available.');
 }
 
 module.exports = getRandomValues;
